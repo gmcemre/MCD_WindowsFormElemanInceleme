28bc90503eeafbde2b21abadad1bc1dd8e87c45e
emre gemici <gmcc.emree@gmail.com>
2021-12-10 15:47:16 +0300

    code changed

--- MCD_WindowsFormElemanInceleme/Form1.Designer.cs
+++ MCD_WindowsFormElemanInceleme/Form1.Designer.cs
@@ -31,10 +31,12 @@ namespace MCD_WindowsFormElemanInceleme
         {
             this.btnResimSec = new System.Windows.Forms.Button();
             this.chkEkGaranti = new System.Windows.Forms.CheckBox();
+            this.listView1 = new System.Windows.Forms.ListView();
             this.SuspendLayout();
             // 
             // btnResimSec
             // 
+            this.btnResimSec.Font = new System.Drawing.Font("Tahoma", 7.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
             this.btnResimSec.Location = new System.Drawing.Point(99, 102);
             this.btnResimSec.Name = "btnResimSec";
             this.btnResimSec.Size = new System.Drawing.Size(114, 37);
@@ -45,18 +47,29 @@ namespace MCD_WindowsFormElemanInceleme
             // chkEkGaranti
             // 
             this.chkEkGaranti.AutoSize = true;
-            this.chkEkGaranti.Location = new System.Drawing.Point(320, 111);
+            this.chkEkGaranti.Font = new System.Drawing.Font("Tahoma", 7.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.chkEkGaranti.Location = new System.Drawing.Point(301, 111);
             this.chkEkGaranti.Name = "chkEkGaranti";
-            this.chkEkGaranti.Size = new System.Drawing.Size(128, 21);
+            this.chkEkGaranti.Size = new System.Drawing.Size(133, 21);
             this.chkEkGaranti.TabIndex = 1;
             this.chkEkGaranti.Text = "Ek Garanti 2 Yıl";
             this.chkEkGaranti.UseVisualStyleBackColor = true;
+            // 
+            // listView1
+            // 
+            this.listView1.HideSelection = false;
+            this.listView1.Location = new System.Drawing.Point(510, 81);
+            this.listView1.Name = "listView1";
+            this.listView1.Size = new System.Drawing.Size(121, 97);
+            this.listView1.TabIndex = 2;
+            this.listView1.UseCompatibleStateImageBehavior = false;
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(811, 646);
+            this.Controls.Add(this.listView1);
             this.Controls.Add(this.chkEkGaranti);
             this.Controls.Add(this.btnResimSec);
             this.Name = "Form1";
@@ -70,6 +83,7 @@ namespace MCD_WindowsFormElemanInceleme
 
         private System.Windows.Forms.Button btnResimSec;
         private System.Windows.Forms.CheckBox chkEkGaranti;
+        private System.Windows.Forms.ListView listView1;
     }
 }
 
